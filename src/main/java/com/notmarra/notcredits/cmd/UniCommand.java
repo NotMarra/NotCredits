ddfc51c05007cee5243fa2dbd6cbdc1b76edec60
NotMarra <marra313cz@gmail.com>
2024-09-17 16:47:08 +0200

    fix #9

--- src/main/java/com/notmarra/notcredits/cmd/UniCommand.java
+++ src/main/java/com/notmarra/notcredits/cmd/UniCommand.java
@@ -12,68 +12,65 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UniCommand {
-    public static void execute(CommandSender sender, String [] args, String operator, String permission, String message_player, String message_admin, String message_invalid) {
-        Boolean isConsole;
-        Player p;
-        if (sender instanceof Player) {
-            p = (Player) sender;
-            isConsole = false;
-        } else {
-            p = null;
-            isConsole = true;
+    public static void execute(CommandSender sender, String[] args, String operator, String permission, String message_player, String message_admin, String message_invalid) {
+        boolean isConsole = !(sender instanceof Player);
+        Player player = isConsole ? null : (Player) sender;
+
+        if (!isConsole && !player.hasPermission(permission)) {
+            Message.sendMessage(player, "no_perm", isConsole, null);
+            return;
         }
 
-        if (!p.hasPermission(permission) && !isConsole) {
-            Message.sendMessage(p, "no_perm", false, null);
-        } else {
-            if (args.length == 3) {
-                Player player = Bukkit.getPlayer(args[1]);
+        if (args.length != 3) {
+            Message.sendMessage(player, message_invalid, isConsole, null);
+            return;
+        }
 
-                Bukkit.getScheduler().runTaskAsynchronously(Notcredits.getInstance(), () -> {
-                    double amount;
-                    if (player != null) {
-                        try {
-                            amount = Double.parseDouble(args[2]);
-                        } catch (NumberFormatException e) {
-                            Message.sendMessage(p, "not_number", isConsole, null);
-                            return;
-                        }
-                        if (amount < 0) {
-                            Message.sendMessage(p, "not_positive_number", isConsole, null);
-                            return;
-                        }
-                        double credits = DatabaseManager.getInstance(Notcredits.getInstance()).getBalance(player.getUniqueId().toString());
+        Player targetPlayer = Bukkit.getPlayer(args[1]);
+        if (targetPlayer == null) {
+            Message.sendMessage(player, "player_not_found", isConsole, null);
+            return;
+        }
 
-                        switch (operator) {
-                            case "add":
-                                credits += amount;
-                                break;
-                            case "remove":
-                                credits -= amount;
-                                break;
-                            case "set":
-                                credits = amount;
-                                break;
-                            default:
-                                Message.sendMessage(p, "invalid_operator", isConsole, null);
-                                break;
-                        }
-
-                        DatabaseManager.getInstance(Notcredits.getInstance()).setBalance(player.getUniqueId().toString(), credits);
-
-                        Map<String, String> replacements = new HashMap<>();
-                        replacements.put("amount", Decimal.formatBalance(amount));
-                        replacements.put("player", player.getName());
-                        Message.sendMessage(p, message_admin, isConsole, replacements);
-                        replacements.remove("player");
-                        Message.sendMessage(player, message_player, isConsole, replacements);
-                    } else {
-                        Message.sendMessage(p, "player_not_found", isConsole, null);
-                    }
-                });
-            } else {
-                Message.sendMessage(p, message_invalid, isConsole, null);
+        Bukkit.getScheduler().runTaskAsynchronously(Notcredits.getInstance(), () -> {
+            double amount;
+            try {
+                amount = Double.parseDouble(args[2]);
+            } catch (NumberFormatException e) {
+                Message.sendMessage(player, "not_number", isConsole, null);
+                return;
             }
-        }
+
+            if (amount < 0) {
+                Message.sendMessage(player, "not_positive_number", isConsole, null);
+                return;
+            }
+
+            double credits = DatabaseManager.getInstance(Notcredits.getInstance()).getBalance(targetPlayer.getUniqueId().toString());
+
+            switch (operator) {
+                case "add":
+                    credits += amount;
+                    break;
+                case "remove":
+                    credits -= amount;
+                    break;
+                case "set":
+                    credits = amount;
+                    break;
+                default:
+                    Message.sendMessage(player, "invalid_operator", isConsole, null);
+                    return;
+            }
+
+            DatabaseManager.getInstance(Notcredits.getInstance()).setBalance(targetPlayer.getUniqueId().toString(), credits);
+
+            Map<String, String> replacements = new HashMap<>();
+            replacements.put("amount", Decimal.formatBalance(amount));
+            replacements.put("player", targetPlayer.getName());
+            Message.sendMessage(player, message_admin, isConsole, replacements);
+            replacements.remove("player");
+            Message.sendMessage(targetPlayer, message_player, false, replacements);
+        });
     }
 }
